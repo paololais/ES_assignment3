ee09523fa90f9c4c04444bc8fa09a6dc43615c9a
FilippoSalterini <s4516129@studenti.unige.it>
2025-03-17 10:38:58 +0100

    register code update ass3

--- main.c
+++ main.c
@@ -10,5 +10,17 @@
 #include "timer.h"
 
 int main(void) {
+    
+    TRISDbits.TRISD11 = 1;
+    RPINR18bits.U1RXR = 75;
+    
+    TRISDbits.TRISD0 = 0;
+    RPOR0bits.RP64R = 1;
+    
+    U1BRG = 11; // (7372800 / 4) / (16 ? 9600)? 1
+    U1MODEbits.UARTEN = 1; // enable UART
+    U1STAbits.UTXEN = 1; // enable U1TX (must be after UARTEN)
+    U1TXREG = ?C?; // send ?C?
+
     return 0;
 }
